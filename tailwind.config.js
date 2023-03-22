84fb13a145b82cb8ccb6979b9d5da2ecd0c0affd
Rogério Santos <rogersanctus@gmail.com>
2023-03-22 18:30:06 -0300

    feat: adds tailwindcss and makes its initial setup feat: setup the fixed background

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable-next-line */
+const { FIT_MENU } = require('./consts')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,css,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sono: ['var(--sono-font)'],
+        inter: ['var(--inter-font)'],
+        poppins: ['var(--poppins-font)'],
+        'josefin-slab': ['var(--josefin-slab-font)']
+      },
+      colors: {
+        primary: '#55dde0',
+        secondary: '#33658a',
+        terciary: '#2f4858',
+        accent: '#f6ae2d',
+        orange: '#f26419',
+        surface: '#eef1f2',
+        'cape-lee': {
+          100: '#dee9ec',
+          200: '#bdd3da',
+          300: '#9dbec7',
+          400: '#7ca8b5',
+          500: '#5c92a2',
+          600: '#518A8F',
+          700: '#497582',
+          800: '#375861',
+          900: '#243a41'
+        },
+        othergreen: '#A9C491'
+      },
+      screens: {
+        fitmenu: `${FIT_MENU}px`
+      }
+    }
+  },
+  plugins: []
+}
