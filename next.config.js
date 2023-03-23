722cec98d3d7d31bea7ad84ce1f8929327765366
Rogério Santos <rogersanctus@gmail.com>
2023-03-23 17:30:34 -0300

    chore: adds and configures the SVGR package to load and inline SVGs inside components as React components

--- next.config.js
+++ next.config.js
@@ -2,6 +2,26 @@
 const nextConfig = {
   experimental: {
     appDir: true
+  },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    )
+
+    config.module.rules.push({
+      ...fileLoaderRule,
+      test: /\.svg$/i,
+      resourceQuery: /url/ // *.svg?url
+    })
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: /url/ }, // exclude if *.svg?url
+      use: ['@svgr/webpack']
+    })
+
+    return config
   }
 }
 
